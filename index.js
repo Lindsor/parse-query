2b176d751e42ab5292a3bfa26ab6096ffa55872b
marcelopluz@gmail.com <marcelopluz@gmail.com>
2016-05-09 18:46:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * Parses the sub query into an obj.
+ * @param  {Object} obj      The object to return.
+ * @param  {String} subQuery Query string in the format key=value.
+ * @return {Object}          The object with the set key value pair.
+ */
+function subParse(obj, subQuery) {
+  obj = obj || {};
+
+  subQuery = subQuery.split("=");
+  var key = decodeURIComponent(subQuery[0]);
+  var val = decodeURIComponent(subQuery[1] || "");
+
+  obj[key] = val;
+
+  return obj;
+}
+
+/**
+ * Cleans the query removing starting '?' or '#' so that location.search and location.hash can be passed in.
+ * @param  {String} query The queryString to clean.
+ * @return {String}       The cleaned queryString.
+ */
+function cleanQuery(query) {
+  query = query || "";
+
+  return query.replace(/^[?#]/g, "");
+}
+
+/**
+ * Parses the pased in querystring returning an object with the values set.
+ * @param  {String} query The query string in the format: key1=val1&key2=val2
+ * @return {Object}       The object with the keys and values set.
+ */
+module.exports = exports = function(query) {
+  var json = {};
+
+  if (!query) return json;
+
+  query = cleanQuery(query);
+
+  json = query.split("&").reduce(subParse, {});
+
+  return json;
+};
